a6251b728edf1fcb97daf1e1bb60ff9089fa9c5f
NakagawaYuto <n.yuuto.11.11@gmail.com>
2024-03-06 13:15:59 +0900

    fix seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-ElderlyPerson.create!(
+elderly_person1 = ElderlyPerson.create(
   name: "John Doe",
   address: "123 Main St, City",
   date_of_birth: Date.new(1950, 5, 20),
@@ -15,7 +15,7 @@ ElderlyPerson.create!(
 )
 
 # 別のElderPersonのデータを追加する
-ElderlyPerson.create!(
+elderly_person2 = ElderlyPerson.create!(
   name: "Jane Smith",
   address: "456 Elm St, Town",
   date_of_birth: Date.new(1945, 9, 10),
@@ -28,13 +28,10 @@ parent1 = Parent.create(name: "Parent 1")
 parent2 = Parent.create(name: "Parent 2")
 
 # Create Children associated with Parents
-Child.create(name: "Child 1", gender: "Male", parent_id: parent1.id, age: 2)
-Child.create(name: "Child 2", gender: "Female", parent_id: parent1.id, age: 3)
-Child.create(name: "Child 3", gender: "Male", parent_id: parent2.id, age: 4)
-Child.create(name: "Child 4", gender: "Female", parent_id: parent2.id, age: 5)
+child1 = Child.create(name: "Child 1", gender: "Male", parent_id: parent1.id, age: 2)
+child2 = Child.create(name: "Child 2", gender: "Female", parent_id: parent1.id, age: 3)
+child3 = Child.create(name: "Child 3", gender: "Male", parent_id: parent2.id, age: 4)
+child4 = Child.create(name: "Child 4", gender: "Female", parent_id: parent2.id, age: 5)
 
-
-バックエンド確認用
-Chat.create(eldery_person_id: 1, parent_id: 1, speaker:"elderly_person", message_content:"メッセージの内容", datetime:DateTime.now)
-Evaluation.create(elderly_person_id: 2, parent_id: 1, evaluation: 3)
-Evaluation.create(elderly_person_id: 2, parent_id: 2, evaluation: 3)
+evaluation1 = Evaluation.create(elderly_person_id: 1, parent_id: 1, evaluation: 3)
+evaluation2 = Evaluation.create(elderly_person_id: 1, parent_id: 2, evaluation: 3)
