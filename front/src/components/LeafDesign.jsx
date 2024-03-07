ebe59b1f114626a199db69606d9c0857208ab645
KusumotoTakahiro <gantaka921@icloud.com>
2024-03-07 14:56:37 +0900

    サクラが舞うデザインの反映

---
+++ front/src/components/LeafDesign.jsx
@@ -0,0 +1,21 @@
+import '../css/sakura.css';
+
+const Sakura = () => {
+  return(
+    <>
+      <ul className="leaf">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  )
+}
+
+export default Sakura;
