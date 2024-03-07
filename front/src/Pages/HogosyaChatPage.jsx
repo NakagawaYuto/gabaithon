60cd9f9aac2750809a3fdc73a5dd4bb515d8fd4b
dorapon48 <dorapon48@icloud.com>
2024-03-07 17:30:17 +0900

    add: じじばば用, 保護者用のチャットページを追加

---
+++ front/src/Pages/HogosyaChatPage.jsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import { 
+  useNavigate,
+  useParams,
+} from 'react-router-dom';
+import axios from "axios"
+
+import { 
+  Grid,
+} from '@mui/material';
+
+import MessageInput from '../components/MessageInputBar';
+import TalkLog from '../components/TalkHistory';
+import MatchEndButton from '../components/MatchEndButton';
+
+const HogosyaChatPage = () => {
+  const params = useParams();
+  const [talklog, setTalkLog] = React.useState([]);
+
+  React.useEffect(() => {
+    //ここでtalklogをDBから取得
+    setTalkLog([
+      {who:'elder', message_text: 'こんにちは'},
+    ])
+    handleGetMessage()
+  }, [])
+
+  const handleSendMessage = async (message) => {
+    const baseURL = 'http://localhost:3000/';
+    
+    try {
+      // axios post
+      await axios.post(baseURL + 'chats', {
+        chats: {
+          eldery_person_id: 1,
+          parent_id: 2,
+          speaker: "parent",
+          message_content: message
+        }
+      })
+      .then((e)=> {
+        console.log(e)
+        setTalkLog([...talklog, {who: 'parent', message_text: message}])
+      })
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleGetMessage() {
+    const baseURL = 'http://localhost:3000/'
+    try {
+      //axios get
+      axios.get(baseURL + 'chats').then((response) => {
+        console.log(response.data);
+        let logs = []
+        response.data.forEach((element) => {
+          logs.push({who: element.speaker, message_text: element.message_content})
+        });
+        setTalkLog(logs)
+          
+      })
+    }
+    catch (error) {
+      console.log(error);
+      return 0
+    }
+  }
+
+  return(
+    <>
+      このChatPageは{params.id}番のページです．
+      <Grid 
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{width:'100%'}}
+      >
+        <Grid
+          item
+          style={{
+            width: '90%', 
+            margin: 0,
+            marginTop: '20px',
+            padding: 0,
+          }}
+        >
+          <TalkLog talklog={talklog} speaker={'parent'}/>
+        </Grid>
+        <Grid item>
+          <MessageInput onSendMessage={handleSendMessage}/>
+        </Grid>
+      </Grid>
+      <MatchEndButton/>
+    </>
+  )
+}
+
+export default HogosyaChatPage;
