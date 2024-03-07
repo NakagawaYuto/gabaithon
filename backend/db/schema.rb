0f44f2ec769189cc4846bcbe0012d3d4643c5e6b
NakagawaYuto <n.yuuto.11.11@gmail.com>
2024-03-07 14:39:10 +0900

    fix chat table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_04_085409) do
     t.integer "parent_id"
     t.string "speaker"
     t.text "message_content"
-    t.datetime "datetime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
