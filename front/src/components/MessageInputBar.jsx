4e03f48b94dc9ec0b3465785fb309a600cf606c3
NakagawaYuto <n.yuuto.11.11@gmail.com>
2024-03-08 03:17:09 +0900

    improve chat ui

--- front/src/components/MessageInputBar.jsx
+++ front/src/components/MessageInputBar.jsx
@@ -1,4 +1,4 @@
-import '../css/messageInput.scss';
+// import '../css/messageInput.scss';
 import { ReactComponent as PaperPlaneIcon } from '../icon/paperPlane.svg';
 import { useState } from 'react';
 
@@ -8,6 +8,68 @@ import {
 
 import MicButton from './MicButton';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+// color変数を宣言
+const messageMainColor = '#f2f2f2';
+const shadowColor = 'rgba(0, 0, 0, 0.1)';
+
+const messageInputContainer = css`
+  position: sticky;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-left: -50px;
+  margin-top: 340px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-right: 40px;
+  padding-left: 40px;
+  background-color: ${messageMainColor};
+  border: 1px solid #ccc;
+  border-radius: 5px;
+
+  /* 影を付ける */
+  box-shadow: 0 2px 4px ${shadowColor};
+
+  .message-input {
+    flex: 1;
+    padding: 5px;
+    border: none;
+    background-color: transparent;
+    outline: none;
+    width: 460px;
+  }
+
+  .send-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    margin-left: 30px;
+    padding: 10px;
+    padding-left: 8px; // iconが非対称であるため，微調整しているだけ
+    padding-right: 11px; // iconが非対称であるため，微調整しているだけ
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 50%;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #0056b3;
+    }
+  }
+
+  .send-button-icon {
+    fill: whitesmoke;
+  }
+`;
+
 
 function MessageInput({ onSendMessage }) {
   const {
@@ -55,27 +117,25 @@ function MessageInput({ onSendMessage }) {
   }
 
   return (
-    <>
-      <div className="message-input-container">
-        <MicButton handleIsMicOn={handleIsMicOn}/>
-        <TextField
-          id="outlined-multiline-flexible"
-          className="text"
-          placeholder=""
-          multiline
-          maxRows={10}
-          value={isMicOn ? currentMessage + transcript : message}
-          onChange={handleMessageChange}
-          onKeyDown={handleKeyPress}
-          fullWidth
-          variant="outlined"
-        />
-        <button className='send-button' onClick={handleSendMessage}>
-          <PaperPlaneIcon className='send-button-icon' />
-        </button>
-      </div>
-    </>
-  )
-}
+    <div css={messageInputContainer}>
+      <MicButton handleIsMicOn={handleIsMicOn}/>
+      <TextField
+        id="outlined-multiline-flexible"
+        className='message-input'
+        placeholder=""
+        multiline
+        maxRows={10}
+        value={isMicOn ? currentMessage + transcript : message}
+        onChange={handleMessageChange}
+        onKeyDown={handleKeyPress}
+        fullWidth
+        variant="outlined"
+      />
+      <button className='send-button' onClick={handleSendMessage}>
+        <PaperPlaneIcon className='send-button-icon' />
+      </button>
+    </div>
+  );
+};
 
 export default MessageInput;
